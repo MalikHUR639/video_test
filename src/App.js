3d6685d4fc32ec2e6b05a9c4cded109bd4cca0b9
afzaalnazir98 <iamafzaalnazir@gmail.com>
2023-11-03 19:46:10 +0500

    player added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Player Example</h1>
+      <ReactPlayer
+      url="https://res.cloudinary.com/dufokqlnr/video/upload/v1699022248/IQLAND-Explainer_v78ted.mp4"
+      width={'100%'}
+      height="auto"
+      controls={true} // Show playback controls
+      playing={true}  // Autoplay the video
+      muted={true}    // Mute the video (required for autoplay in some browsers)
+      loop={true}     // Loop the video
+    />
     </div>
   );
 }
